8975fbb94789cc1be5cfa1ae5da0909ac1e82058
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-12-16 00:27:42 +0800

    Create handleSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 const App = () => {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
   return (
     <div className="App font-poppins">
-      <Navbar />
+      <Navbar search={search} onSearch={handleSearch} />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-const Navbar = () => {
+const Navbar = ({ search, onSearch }) => {
   return (
     <div className="border-b py-4 flex items-baseline">
       <div className="ml-4 font-semibold text-green-700">Project X</div>
       <div>
-        <input type="text" placeholder="Search ID..." className="ml-8 border rounded-lg p-2" />
+        <input
+          className="ml-8 border rounded-lg p-2"
+          type="text"
+          placeholder="Search ID..."
+          value={search}
+          onChange={onSearch}
+        />
       </div>
     </div>
   )
