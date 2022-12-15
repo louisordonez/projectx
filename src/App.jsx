b953dc98da0eb3dce857604f6c2255190249070b
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-12-15 23:55:11 +0800

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar'
+
 const App = () => {
-  return <div className="App font-poppins"></div>
+  return (
+    <div className="App font-poppins">
+      <Navbar />
+    </div>
+  )
 }
 
 export default App
