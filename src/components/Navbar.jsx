2e36e2e17f5ef6a46637cf3e0ee43195ec3bab1b
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-12-16 00:17:04 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="border-b py-4 flex items-baseline">
+      <div className="ml-4 font-semibold text-green-700">Project X</div>
+      <div>
+        <input type="text" placeholder="Search ID..." className="ml-8 border rounded-lg p-2" />
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
