5e800e57241184318e8dffdf26525bab2dc3db7e
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-12-16 16:59:37 +0800

    Create spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Table from './components/Table'
+import Spinner from './components/Spinner'
 import useMarkets from './services/hooks/useMarkets'
 
 const App = () => {
@@ -24,7 +25,9 @@ const App = () => {
         <h1>Markets</h1>
       </div>
       {loading ? (
-        'Loading...'
+        <div className="mt-8 mb-8">
+          <Spinner />
+        </div>
       ) : (
         <div className="mt-8 mb-8">
           <Table markets={markets} page={page} onNextPage={handleNextPage} onPreviousPage={handlePreviousPage} />
